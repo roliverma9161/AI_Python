31d1100789d1b9f36c346214c977cb4d649cd8a1
roliverma9161 <roliverma9161@gmail.com>
2026-08-16 21:04:54 +0530

    aggregatefunction.py

---
+++ aggregatefunction.py
@@ -0,0 +1,15 @@
+import numpy as np
+A1=np.array([1,2,3,4,5])
+A2=np.array([[1,2,3],[4,5,6]])
+A3=np.array([[1,2,3],[4,5,6],[7,8,9]])
+#1D Array - A1
+print(np.prod(A1))
+#2D Array - A2
+print(np.prod(A2))
+print(np.prod(A2,0))
+print(np.prod(A2,1))
+
+#2D Array - A3
+print(np.prod(A3))
+print(np.prod(A3,0))
+print(np.prod(A3,1))
