419a7776dfadcb921668cffe80833635beb48bae
roliverma9161 <roliverma9161@gmail.com>
2026-08-17 20:24:28 +0530

    arangefunction.py

---
+++ arangefunction.py
@@ -0,0 +1,8 @@
+import numpy as np
+print("A1\n",np.arange(4).reshape(2,2),"\n")
+print("A2\n",np.arange(4,10),"\n")
+print("A3\n",np.arange(4,20,3),"\n")
+
+#printing all numbers from 1 to 2 in steps of 0.1
+print("A4\n",np.arange(1,2,0.1))
+
