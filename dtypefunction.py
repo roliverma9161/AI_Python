a288c86a8349fdc1b5f48f0656802013ee7c3917
roliverma9161 <roliverma9161@gmail.com>
2026-08-19 21:55:10 +0530

    dtypefunction.py

---
+++ dtypefunction.py
@@ -0,0 +1,20 @@
+import numpy as np
+a=np.array([1,2,3,4,5])
+print('values of Array')
+print(a)
+print('Data type of Array:',a.dtype)
+
+a=np.array([1,2,3,4,5],dtype='i2')
+print('values of Array')
+print(a)
+print('Data type of Array:',a.dtype)
+
+a=np.array([1,2,3,4,5],dtype='S')
+print('values of Array')
+print(a)
+print('Data type of Array:',a.dtype)
+
+a=np.array([1,2,3,4,5],dtype='f')
+print('values of Array')
+print(a)
+print('Data type of Array:',a.dtype)
