f3d14f79b4c1bf81f70d5dbc5a46b44dad0701b6
roliverma9161 <roliverma9161@gmail.com>
2026-08-19 21:58:29 +0530

    listmanipu.py

---
+++ listmanipu.py
@@ -0,0 +1,14 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> list1=['physics','chemistry',1997,2000]
+... print("list1[1:3]:",list1[1:3])
+... print("list1[1:5:2]:",list1[1:5:2])
+... 
+... #negative index
+... ist1=['physics','chemistry',1997,2000]
+... print("list1[-3:-1]:",list1[-3:-1])
+... print("list1[-3:-1:2]",list1[-3:-1:2])
+... 
+... #loop through a list
+... list=['physics','chemistry',1997,2000]
+... for x in list:
