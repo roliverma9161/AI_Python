5eb803f8cac1ede8473ec07fe6d5e3159efe5a45
roliverma9161 <roliverma9161@gmail.com>
2026-08-20 14:38:23 +0530

    listmanipulation.py

---
+++ listmanipulation.py
@@ -0,0 +1,13 @@
+list1=['physics','chemistry',1997,2000]
+print("list1[1:3]:",list1[1:3])
+print("list1[1:5:2]:",list1[1:5:2])
+
+#negative index  
+ist1=['physics','chemistry',1997,2000]
+print("list1[-3:-1]:",list1[-3:-1])
+print("list1[-3:-1:2]",list1[-3:-1:2])
+
+#loop through a list
+list=['physics','chemistry',1997,2000]
+for x in list:
+    print(x)
