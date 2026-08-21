3ca2e0e8b2599ca4d89073375ae33cf7fa2eb1f3
roliverma9161 <roliverma9161@gmail.com>
2026-08-21 21:37:32 +0530

    m9.py

---
+++ m9.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+# data to plot
+n_groups = 4
+Sales_Voltas = (90, 55, 40, 65)
+Sales_Samsung = (85, 62, 54, 20)
+Year_Sales= ('2016', '2017', '2018', '2020')
+# create plot
+y_pos= np.arange(n_groups)
+bar_width = 0.35
+opacity = 0.8
+
