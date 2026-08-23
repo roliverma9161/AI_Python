2df05fbc07e0c377bfc4e5a15a2259e558a78cf6
roliverma9161 <roliverma9161@gmail.com>
2026-08-24 00:05:07 +0530

    numpydatatype.py

---
+++ numpydatatype.py
@@ -0,0 +1,7 @@
+import numpy as np
+a=np.array([1,2,3,4,5])
+a=np.array([1,2,3,4,5],dtype='i2')
+a=np.array([1,2,3,4,5],dtype='S')
+a=np.array([1,2,3,4,5],dtype='f')
+
+print('data type of Array:',a.dtype)
