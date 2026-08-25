b4e6b37d0da6587045667fccf447b2b03f11c34f
roliverma9161 <roliverma9161@gmail.com>
2026-08-25 21:14:56 +0530

    searchingsorting.py

---
+++ searchingsorting.py
@@ -0,0 +1,9 @@
+import numpy as np
+a=np.array([1,4,3,2,8,7,6])
+print('values of Array-A')
+print(a)
+print('sorted Array:')
+print(np.sort(a))
+print('search the even number')
+x=np.where(a%2==0)
+print(x)
