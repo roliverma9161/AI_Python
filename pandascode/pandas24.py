ce4c2b8cb72b86f63d3db4e1925d802a822f87ea
roliverma9161 <roliverma9161@gmail.com>
2026-09-04 20:35:12 +0530

    pandas24.py

---
+++ pandascode/pandas24.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+#dictionary of lists
+dict={'First Score':[100,90,np.nan,95],
+      'Second Score':[30,40,56,np.nan],
+      'Third Score':[np.nan,40,80,98]}
+#creating a dataframe using list
+df=pd.DataFrame(dict)
+
+#using null()function
+df.null()
