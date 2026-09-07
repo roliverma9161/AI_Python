e3037c9381fdb17a34a125132b90e530ba0609fe
roliverma9161 <roliverma9161@gmail.com>
2026-09-07 20:48:24 +0530

    pandas34.py

---
+++ pandascode/pandas34.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+#dictionary of lists
+dict={'First Score':[100,90,87,95],
+      'Second Score':[30,40,56,np.nan],
+      'Third Score':[56,40,80,98],
+      'Forth Score':[np.nan,np.nan,87,np.nan]}
+
+#creating a dataframe from dictionary
+df=pd.DataFrame(dict)
+
+#using dropna()function
+print(df.dropna(axis=1))
